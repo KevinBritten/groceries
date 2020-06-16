8783ea988a36f8437242224c2e43e27ab138e8fb
Kevin <kevinjbritten@gmail.com>
2020-06-16 11:01:17 -0400

    started stionks feature

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
-let forum = document.getElementById("forum")
+let forum = document.getElementById('forum');
 
 let makeList = function(str) {
-    let numberOfItems = str.match(/[$]/g).length / 2
-    let items = []
-    let sortedItems = []
+    let remainingItems = str;
+    let numberOfItems = str.match(/[$]/g).length / 2;
+    let items = [];
+    let sortedItems = [];
     for (let i = 0; i < numberOfItems; i++) {
-        let secondDollar = str.indexOf("$", (str.indexOf("$") + 1)) + 6
-        items.push(str.slice(0, secondDollar))
+        let secondDollar = remainingItems.indexOf('$', remainingItems.indexOf('$') + 1) + 6;
+        items.push(remainingItems.slice(0, secondDollar));
+        remainingItems = remainingItems.slice(secondDollar);
     }
     for (let item of items) {
         let currentObject = {
             name: item.slice(0, item.indexOf('Qty:')),
-            price: item.slice(item.indexOf("$", (item.indexOf("$") + 1)))
-        }
-        sortedItems.push(currentObject)
+            price: item.slice(item.indexOf('$', item.indexOf('$') + 1))
+        };
+        sortedItems.push(currentObject);
     }
     sortedItems.forEach((item) => {
-        let row = document.createElement("tr")
-        console.log(forum)
-        document.getElementById("forum").appendChild(row)
-        forum.lastChildElement.innerHtml = `<td>${item['name']}</td><td>${item['price']}</td>`
-    })
-}
+        let row = document.createElement('tr');
+        document.getElementById('forum').appendChild(row);
+        let edit = forum.lastElementChild;
+        let currentPrice = parseFloat(item['price'].slice(1));
+        edit.innerHTML = `<td class="name">${item['name']}</td>
+        <td class="price" price="${currentPrice}">${item['price']}</td>
+        <td><input type="checkbox" class="kevan"></td>
+        <td><input type="checkbox" class="david"></td>
+        <td><input type="checkbox" class="eric"></td>
+        <td><input type="checkbox" class="evan"></td>
+        <td><input type="checkbox" class="duncan"></td>
+        `;
+    });
+};
+
+let stonks = function() {
+    let kevanTot = 0;
+    let davidTot = 0;
+    let ericTot = 0;
+    let evanTot = 0;
+    let duncanTot = 0;
+    let items = forum.children;
+    for (let tr of items) {
+        for (let td of tr.children) {
+            let itemPrice = 0;
+            if (td.classList.contains('price')) {
+                itemPrice = td.getAttribute('price');
+            }
+        }
+    }
+};
 
 makeList(`Granulated Sugar
 20145033_EA
@@ -50,4 +77,6 @@ $2.88
 Mango
 20059635001_EA
 Qty: 3 @ $0.88 ea
-$2.64`)
+$2.64`);
+
+stonks();
